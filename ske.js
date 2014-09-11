423847e42479c4ae8ec6c40dd55bc1c9e3cf28f6
lhj73999 <ehgkfdl@naver.com>
2014-09-11 16:03:28 +0900

    ddd

--- ske.js
+++ ske.js
@@ -1,17 +1,23 @@
 function setup() {
   createCanvas(640, 480);
-  background(128, 0, 64);
+  
   noFill();
 
   stroke(255, 0, 0);
- function drow() {
-  
+
+}
+
+function draw () {
+
+   background(128, 0, 64);
 
 for (var x = 1; x <= 8; x++)
 {
-  ellipse(x * 70, x * 20 + 100, 50, 50);
+  ellipse(x * 70, x * 20 + 100, x+50, x*10);
 }
+
+var size =    Math.abs(mouseX - 320);
+
+ellipse(320, 240, mouseX, mouseY);
+
 }
- 
-var size = Math, abs(mouseX=320);
- }
